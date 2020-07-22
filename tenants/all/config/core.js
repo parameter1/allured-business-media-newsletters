1a4f2cb36b3d726bfad57c8e7f74645a1f4684b6
Josh Worden <josh@limit0.io>
2020-07-22 16:01:22 -0500

    add nailpro daily section ids

--- tenants/all/config/core.js
+++ tenants/all/config/core.js
@@ -1,1 +1,15 @@
-module.exports = {};
+module.exports = {
+  sectionIds: {
+    'nailpro-daily': {
+      'featured-article': 30699,
+      'ad-primary': 30718,
+      'industry-news': 30700,
+      'ad-secondary': 30719,
+      'new-products': 30701,
+      'ad-tertiary': 30720,
+      'cant-miss': 30702,
+      'ad-quaternary': 30721,
+      partnerships: 30703,
+    },
+  },
+};
