6cfc66a427adfe436f1ca1ce7d2cd7fc9a89dea8
Brandon Krigbaum <brandonbk@gmail.com>
2020-08-05 15:46:43 -0500

    Update NP ad unit names

--- tenants/all/config/email-x.js
+++ tenants/all/config/email-x.js
@@ -14,7 +14,7 @@ config
 
   .setAdUnits('np-newsletter', [
     {
-      name: 'primary',
+      name: 'ad-slot-1',
       id: '5f187f86b484c91cd9d5b6b1',
       width: 300,
       height: 250,
@@ -23,7 +23,7 @@ config
 
   .setAdUnits('np-newsletter', [
     {
-      name: 'secondary',
+      name: 'ad-slot-2',
       id: '5f187f92b484c95e9ad5b6cd',
       width: 300,
       height: 250,
@@ -32,7 +32,7 @@ config
 
   .setAdUnits('np-newsletter', [
     {
-      name: 'tertiary',
+      name: 'ad-slot-3',
       id: '5f187f9fb484c9db95d5b6e9',
       width: 300,
       height: 250,
@@ -41,7 +41,7 @@ config
 
   .setAdUnits('np-newsletter', [
     {
-      name: 'quaternary',
+      name: 'ad-slot-4',
       id: '5f187fafb484c98791d5b717',
       width: 300,
       height: 250,
