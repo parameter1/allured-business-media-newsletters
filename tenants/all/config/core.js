107358fe15cb4a4483a3b31f2e5b5b7861ea7044
Brandon Krigbaum <brandonbk@gmail.com>
2020-09-14 13:14:50 -0500

    Enable dpm for emailx and content

--- tenants/all/config/core.js
+++ tenants/all/config/core.js
@@ -1,15 +1,10 @@
 module.exports = {
-  sectionIds: {
-    'nailpro-daily': {
-      'featured-article': 64309,
-      'ad-primary': 64315,
-      'industry-news': 64311,
-      'ad-secondary': 64316,
-      'new-products': 64312,
-      'ad-tertiary': 64317,
-      'cant-miss': 64313,
-      'ad-quaternary': 64318,
-      partnerships: 64314,
+  dpm: {
+    emailx: {
+      enabled: true,
+    },
+    content: {
+      enabled: true,
     },
   },
 };
