fbc4ecdb47ea835419aecd2eb4f95b14f02e4ec4
Brandon Krigbaum <brandonbk@gmail.com>
2021-05-05 10:05:31 -0500

    Initial addition of native-x config

---
+++ tenants/all/config/native-x.js
@@ -0,0 +1,53 @@
+module.exports = {
+  uri: 'https://allured.native-x.parameter1.com',
+  placements: {
+    'blp-newsletter': {
+      'ad-slot-1': '',
+      'ad-slot-2': '',
+      'ad-slot-3': '',
+      'ad-slot-4': '',
+    },
+    'ct-newsletter': {
+      'ad-slot-1': '',
+      'ad-slot-2': '',
+      'ad-slot-3': '',
+      'ad-slot-4': '',
+    },
+    'ds-newsletter': {
+      'ad-slot-1': '',
+      'ad-slot-2': '',
+      'ad-slot-3': '',
+      'ad-slot-4': '',
+    },
+    'gci-newsletter': {
+      'ad-slot-1': '',
+      'ad-slot-2': '',
+      'ad-slot-3': '',
+      'ad-slot-4': '',
+    },
+    'me-newsletter': {
+      'ad-slot-1': '',
+      'ad-slot-2': '',
+      'ad-slot-3': '',
+      'ad-slot-4': '',
+    },
+    'np-newsletter': {
+      'ad-slot-1': '',
+      'ad-slot-2': '',
+      'ad-slot-3': '',
+      'ad-slot-4': '',
+    },
+    'pf-newsletter': {
+      'ad-slot-1': '',
+      'ad-slot-2': '',
+      'ad-slot-3': '',
+      'ad-slot-4': '',
+    },
+    'si-newsletter': {
+      'ad-slot-1': '',
+      'ad-slot-2': '',
+      'ad-slot-3': '',
+      'ad-slot-4': '',
+    },
+  },
+};
